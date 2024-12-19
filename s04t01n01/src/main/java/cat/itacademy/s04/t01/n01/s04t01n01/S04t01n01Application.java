e9f02b6071687068bf25e7a42f26639cdc69867c
RustyGearBox <gearbox.bit@gmail.com>
2024-12-20 00:03:17 +0100

    created springboot-maven project

---
+++ s04t01n01/src/main/java/cat/itacademy/s04/t01/n01/s04t01n01/S04t01n01Application.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t01.n01.s04t01n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04t01n01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04t01n01Application.class, args);
+	}
+
+}
---
+++ s04t01n01/src/test/java/cat/itacademy/s04/t01/n01/s04t01n01/S04t01n01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t01.n01.s04t01n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04t01n01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
