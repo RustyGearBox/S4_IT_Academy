87239c7c0d44d1362cab15a4c7c6d727ada1995c
RustyGearBox <gearbox.bit@gmail.com>
2024-12-20 00:40:52 +0100

    t1 lvl1 - completed

---
+++ s04t01n01/src/main/java/cat/itacademy/s04/t01/n01/s04t01n01/Controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package cat.itacademy.s04.t01.n01.s04t01n01.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/greet1")
+    private String greet_1(@RequestParam(value = "name", defaultValue = "Unknown") String name){
+        String saludo = "Hello, " + name + ". You are executing a maven project.";
+        return saludo;
+    }
+
+    @GetMapping("/greet2")
+    private String greet_2(@PathVariable(value = "name", required = false) String name){
+        String saludo = "Hello, " + name + ". You are executing a maven project.";
+        return saludo;
+    }
+
+}
