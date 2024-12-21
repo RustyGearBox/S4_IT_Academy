09da9434782e6fda90148404d25d881ec9785f00
RustyGearBox <gearbox.bit@gmail.com>
2024-12-21 21:44:23 +0100

    lvl: 1-2 code complete

---
+++ S4 Spring 1 - Lvl2/src/main/java/cat/itacademy/s04/t01/n02/s04t01n02/S04t01n02Application.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t01.n02.s04t01n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04t01n02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04t01n02Application.class, args);
+	}
+
+}
