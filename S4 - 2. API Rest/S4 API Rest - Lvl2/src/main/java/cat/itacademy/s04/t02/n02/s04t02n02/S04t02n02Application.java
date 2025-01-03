d09dcee36550de42b53984b92b9fd8dfb3efad1d
RustyGearBox <gearbox.bit@gmail.com>
2025-01-03 19:01:19 +0100

    API Rest - Lvl 2 Completed

---
+++ S4 - 2. API Rest/S4 API Rest - Lvl2/src/main/java/cat/itacademy/s04/t02/n02/s04t02n02/S04t02n02Application.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n02.s04t02n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04t02n02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04t02n02Application.class, args);
+	}
+
+}
---
+++ S4 - 2. API Rest/S4 API Rest - Lvl2/src/main/java/cat/itacademy/s04/t02/n02/s04t02n02/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package cat.itacademy.s04.t02.n02.s04t02n02.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    
+}
